318a4031cccfdef9235764e2ad594295878ac673
Can <cankushbjk@gmail.com>
2017-05-21 23:33:11 +0200

    final version as of 23.33 - 20170521.

---
+++ C1/ex1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+void main (int argc, char *argv[]) 
+{
+	int distance = 0x64;
+	// this is also a comment.
+	printf("You are %d miles away.\n", distance);
+}
---
+++ C1/ex1.s
@@ -0,0 +1,16 @@
+# 1 "ex1.c"
+# 1 "/home/can/Desktop/LCTHW/C1//"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "ex1.c"
+
+
+void main (int argc, char *argv[])
+{
+ int distance = 0x64;
+
+ printf("You are %d miles away.\n");
+}
